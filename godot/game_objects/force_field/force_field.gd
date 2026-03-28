9740d7a163363c317ee16308321bfecae5f5903f
danitheturtle <danitheturtle@pm.me>
2026-03-28 17:02:59 -0400

    force field updates. consistent toggling in all states

--- godot/game_objects/force_field/force_field.gd
+++ godot/game_objects/force_field/force_field.gd
@@ -6,13 +6,15 @@ class_name ForceField
 const ForceFieldEditorHelperRes = preload("res://game_objects/force_field/force_field_editor_helper.gd")
 var editorHelper: ForceFieldEditorHelper = null
 
-@onready var verticalPlayerEmitter = $VerticalPlayerEmitter
-@onready var playerCollider = $VerticalPlayerEmitter/Collider
-@onready var playerVisualField = $VerticalPlayerEmitter/VisualField
-@onready var horizontalPetEmitter = $HorizontalPetEmitter
-@onready var petCollider = $HorizontalPetEmitter/Collider
-@onready var petVisualField = $HorizontalPetEmitter/VisualField
-@onready var indicatorMesh = $IndicatorMesh
+@onready var verticalPlayerEmitter: MeshInstance3D = $VerticalPlayerEmitter
+@onready var playerCollider: StaticBody3D = $VerticalPlayerEmitter/Collider
+@onready var playerVisualField: Node3D = $VerticalPlayerEmitter/VisualField
+@onready var horizontalPetEmitter: MeshInstance3D = $HorizontalPetEmitter
+@onready var petCollider: StaticBody3D = $HorizontalPetEmitter/Collider
+@onready var petVisualField: Node3D = $HorizontalPetEmitter/VisualField
+@onready var indicatorMesh: MeshInstance3D = $IndicatorMesh
+@onready var playerIndicatorMesh: MeshInstance3D = $VerticalPlayerEmitter/PlayerIndicatorMesh
+@onready var petIndicatorMesh: MeshInstance3D = $HorizontalPetEmitter/PetIndicatorMesh
 
 # activator or puzzle wire
 @export_category("Activation")
@@ -24,21 +26,21 @@ var editorHelper: ForceFieldEditorHelper = null
 @export var petFieldActivator: Node = null:
     set(new_value):
         petFieldActivator = new_value
-        if new_value == null: initialPlayerFieldState = false
-        if editorHelper != null: enable_player_field(new_value != null)
+        if new_value == null: initialPetFieldState = false
+        if editorHelper != null: enable_pet_field(new_value != null)
 @export var inactiveColor: Color = Color("be8b26") # TODO refactor into color system
 @export var activeColor: Color = Color("4b9fce") # TODO refactor into color system
 @export_category("Initial State")
 @export var initialPlayerFieldState: bool = true:
     set(new_value):
-        if playerFieldActivator == null: return
-        initialPlayerFieldState = new_value
-        if editorHelper != null: player_field_toggle(new_value)
+        initialPlayerFieldState = playerFieldActivator != null && new_value
+        playerFieldState = initialPlayerFieldState
+        if editorHelper != null: player_field_toggle(initialPlayerFieldState)
 @export var initialPetFieldState: bool = true:
     set(new_value):
-        if petFieldActivator == null: return
-        initialPetFieldState = new_value
-        if editorHelper != null: pet_field_toggle(new_value)
+        initialPetFieldState = petFieldActivator != null && new_value
+        petFieldState = initialPetFieldState
+        if editorHelper != null: pet_field_toggle(initialPetFieldState)
 @export_category("Force Field Size")
 @export_range(1.0, 100.0, 0.5, "or_greater") var fieldWidth: float = 5.5:
     set(new_value):
@@ -50,72 +52,66 @@ var editorHelper: ForceFieldEditorHelper = null
         fieldHeight = new_value
 
 # local state
-var active: bool = false
-var playerFieldState: bool = true
-var petFieldState: bool = true
+var playerFieldState: bool = false
+var petFieldState: bool = false
 
 func _ready() -> void:
+    enable_player_field(playerFieldActivator != null)
+    player_field_toggle(initialPlayerFieldState)
+    enable_pet_field(petFieldActivator != null)
+    pet_field_toggle(initialPetFieldState)
     if Engine.is_editor_hint():
-        after_ready.call_deferred()
+        create_editor_helper.call_deferred()
     else:
-        enable_player_field(playerFieldActivator != null)
-        player_field_toggle(initialPlayerFieldState)
-        enable_pet_field(petFieldActivator != null)
-        pet_field_toggle(initialPetFieldState)
         if playerFieldActivator != null:
-            playerFieldActivator.wire_high.connect(activate_player_field)
-            playerFieldActivator.wire_low.connect(deactivate_player_field)
+            playerFieldActivator.wire_high.connect(on_player_wire_high)
+            playerFieldActivator.wire_low.connect(on_player_wire_low)
         else:
             verticalPlayerEmitter.free()
         if petFieldActivator != null:
-            playerFieldActivator.wire_high.connect(activate_player_field)
-            playerFieldActivator.wire_low.connect(deactivate_player_field)
+            petFieldActivator.wire_high.connect(on_pet_wire_high)
+            petFieldActivator.wire_low.connect(on_pet_wire_low)
         else:
             horizontalPetEmitter.free()
     playerFieldState = playerFieldActivator != null && initialPlayerFieldState
     petFieldState = petFieldActivator != null && initialPetFieldState
 
-func after_ready() -> void:
+func create_editor_helper() -> void:
     editorHelper = ForceFieldEditorHelperRes.new(self)
 
 func _process(delta) -> void:
     if editorHelper != null:
         editorHelper.update(delta)
-    else:
-        # Code to execute in game.
-        pass
 
-func activate_player_field() -> void:
-    set_color(activeColor)
-    playerFieldState = !initialPlayerFieldState
-    player_field_toggle(playerFieldState)
+func on_player_wire_high() -> void: player_field_toggle(!initialPlayerFieldState)
 
-func deactivate_player_field() -> void:
-    set_color(inactiveColor)
-    playerFieldState = initialPlayerFieldState
-    player_field_toggle(playerFieldState)
+func on_player_wire_low() -> void: player_field_toggle(initialPlayerFieldState)
 
-func activate_pet_field() -> void:
-    set_color(activeColor)
-    petFieldState = !initialPetFieldState
-    pet_field_toggle(playerFieldState)
+func on_pet_wire_high() -> void: pet_field_toggle(!initialPetFieldState)
 
-func deactivate_pet_field() -> void:
-    set_color(inactiveColor)
-    petFieldState = initialPetFieldState
-    pet_field_toggle(petFieldState)
+func on_pet_wire_low() -> void: pet_field_toggle(initialPetFieldState)
 
 func player_field_toggle(_on: bool):
     # TODO animation
-    if _on: playerVisualField.show()
-    else: playerVisualField.hide()
-    playerCollider.process_mode = Node.PROCESS_MODE_INHERIT if _on else Node.PROCESS_MODE_DISABLED
+    playerFieldState = _on
+    update_indicator_colors()
+    if _on:
+        playerVisualField.show()
+        playerCollider.process_mode = Node.PROCESS_MODE_INHERIT
+    else:
+        playerVisualField.hide()
+        playerCollider.process_mode = Node.PROCESS_MODE_DISABLED
 
 func pet_field_toggle(_on: bool):
     # TODO animation
-    if _on: petVisualField.show()
-    else: petVisualField.hide()
-    petCollider.process_mode = Node.PROCESS_MODE_INHERIT if _on else Node.PROCESS_MODE_DISABLED
+    petFieldState = _on
+    update_indicator_colors()
+    if _on:
+        petVisualField.show()
+        petCollider.process_mode = Node.PROCESS_MODE_INHERIT
+    else:
+        petVisualField.hide()
+        petCollider.process_mode = Node.PROCESS_MODE_DISABLED
 
 func enable_player_field(_enable: bool):
     if _enable:
@@ -133,6 +129,21 @@ func enable_pet_field(_enable: bool):
         horizontalPetEmitter.hide()
         horizontalPetEmitter.process_mode = Node.PROCESS_MODE_DISABLED
 
-func set_color(newColor: Color) -> void:
-    indicatorMesh.get_surface_override_material(0).albedo_color = newColor
-    indicatorMesh.get_surface_override_material(0).emission = newColor
+func update_indicator_colors() -> void:
+    var isPlayerWireHigh = playerFieldState != initialPlayerFieldState
+    var isPetWireHigh = petFieldState != initialPetFieldState
+    var nextSharedColor = activeColor if isPlayerWireHigh || isPetWireHigh else inactiveColor
+    update_mesh_color(nextSharedColor, indicatorMesh)
+    if playerFieldActivator != null:
+        var nextPlayerColor = activeColor if isPlayerWireHigh else inactiveColor
+        update_mesh_color(nextPlayerColor, playerIndicatorMesh)
+    if petFieldActivator != null:
+        var nextPetColor = activeColor if isPetWireHigh else inactiveColor
+        update_mesh_color(nextPetColor, petIndicatorMesh)
+
+func update_mesh_color(newColor: Color, mesh: MeshInstance3D) -> void:
+    var surfaceMaterial = mesh.get_surface_override_material(0)
+    if surfaceMaterial.albedo_color != newColor:
+        surfaceMaterial.albedo_color = newColor
+    if surfaceMaterial.emission != newColor:
+        surfaceMaterial.emission = newColor
